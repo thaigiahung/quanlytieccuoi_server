60561a0615c2a70ba0e7600676ddaa6c86ed3646
Gia Hung Thai <hung.tg89@gmail.com>
2014-10-31 16:16:07 +0700

    Sửa output API Scan

--- scan.php
+++ scan.php
@@ -8,20 +8,18 @@
 
 	//Đánh dấu khách đã đến
 	$sql = "UPDATE `guest_table` SET status = 1 WHERE id_guest = ".$code;
-	echo $sql."<br>";
 	DataProvider::ExecuteQuery($sql);
 
 	//Tìm bàn của khách này
-	$sql = "SELECT t.* 
+	$sql = "SELECT t.*, g.name AS guest_name
 			FROM `guest_table` gt LEFT JOIN `table` t ON t.id = gt.id_table 
+			LEFT JOIN `guest` g ON g.id = gt.id_guest 
 			WHERE gt.id_guest = ".$code;
-			echo $sql."<br>";
 	$result = DataProvider::ExecuteQuery($sql);
 	$table = mysql_fetch_array($result);
 
 	//Đếm số lượng khách đã đến của bàn đó
 	$sql = "SELECT COUNT(*) AS num_of_guest FROM `guest_table` WHERE status = 1 AND id_table = ".$table['id'];
-	echo $sql."<br>";
 	$result = DataProvider::ExecuteQuery($sql);
 	$row = mysql_fetch_array($result);
 
@@ -31,8 +29,7 @@
 
 	//Cập nhật current_num_of_guest của bàn đó
 	$sql = "UPDATE `table` SET `current_num_of_guest`=".$row['num_of_guest'].",`status`=$status WHERE id = ".$table['id'];
-	echo $sql."<br>";
 	DataProvider::ExecuteQuery($sql);
 
-	echo "Xong";
+	echo json_encode(array("name"=>$table['guest_name'],"table"=>$table['name']));
 ?>
