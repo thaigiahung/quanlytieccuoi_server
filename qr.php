6ab16c040a2a03e38a0e3fd236378ff6230c7bc4
Gia Hung Thai <hung.tg89@gmail.com>
2014-11-23 19:24:05 +0700

    Đổi QR Code thành url thay cho text

--- qr.php
+++ qr.php
@@ -1,7 +1,7 @@
 <?php 
 	for($i = 1; $i <= 500; $i++)
 	{
-		$data="size=40&data=$i&dataType=text&color=156B07&icon=www-2&imageType=custom&imageUrl=http%3A%2F%2Fwww.qrcode-monkey.com%2Fuploads%2F35fea2141b0d6547cd0747dc482583a9.png&effect=0";
+		$data="size=40&data=http%3A%2F%2Fgiahung.net%2F%23$i&dataType=url&color=156B07&icon=www-2&imageType=custom&imageUrl=http%3A%2F%2Fwww.qrcode-monkey.com%2Fuploads%2F75d1a029f9c5699079726ef6b33117e1.png&effect=0";
 		$url="http://www.qrcode-monkey.com/qr-code-generator.php";
 
 		$ch = curl_init();
@@ -14,9 +14,9 @@
 
 		$start = strpos($result, 'temp');
 		$end = strpos($result, '.png')+4;
+		echo $start."\n".$end."\n";
 		$img = substr($result, $start, $end-$start);
 		$img = str_replace('\\', '', $img);
-
 		echo "Downloading ".$i."\n";
 		
 		//Download
