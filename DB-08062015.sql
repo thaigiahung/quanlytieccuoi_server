cf462779feb09615e8397717fba0bb41a782d068
Gia Hung Thai <hung.tg89@gmail.com>
2015-06-08 23:44:17 +0700

    Update DB

--- DB-08062015.sql
+++ DB-08062015.sql
@@ -16,7 +16,11 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
-
+--
+-- Database: `quan_ly_tiec_cuoi`
+--
+CREATE DATABASE IF NOT EXISTS `quan_ly_tiec_cuoi` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `quan_ly_tiec_cuoi`;
 
 -- --------------------------------------------------------
 
